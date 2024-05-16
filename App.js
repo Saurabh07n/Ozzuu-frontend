3d40532b3161c40d13482d42a17f617ba0797fa8
saurabh-zngit <s.n@ambientetechnologies.com>
2024-05-16 22:50:04 +0530

    home screen txt

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { AppProvider } from "./AppContext";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+import { useChatClient } from './useChatClient';
+// import { OverlayProvider } from 'stream-chat-react-native';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const HomeScreen = () => <Text>Home Screen</Text>;
+
+const NavigationStack = () => {
+  const { clientIsReady } = useChatClient();
+
+  if (!clientIsReady) {
+    return <Text>Loading chat ...</Text>
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <OverlayProvider>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
+    // </OverlayProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return (
+    <AppProvider>
+      <SafeAreaProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <SafeAreaView style={{ flex: 1 }}>
+            <NavigationContainer>
+              <NavigationStack />
+            </NavigationContainer>
+          </SafeAreaView>
+          </GestureHandlerRootView>
+      </SafeAreaProvider>
+    </AppProvider>
+
+  );
+};
---
+++ chatConfig.js
@@ -0,0 +1,6 @@
+// chatConfig.js
+export const chatApiKey = 'wq3auamuyb69';
+export const chatUserId = 'jolly-sound-9';
+export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9sbHktc291bmQtOSJ9.d3SwV2IVeVFWrWkfptch6fs7q8BfT9ulQRN-MbeCwiQ';
+export const chatUserName = 'jolly-sound-9';
+
